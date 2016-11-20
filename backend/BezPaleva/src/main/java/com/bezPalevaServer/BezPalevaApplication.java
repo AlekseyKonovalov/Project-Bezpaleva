7e1e973fadc9de6ea5a7fac29211bee4de421a1a
Adgam <adgam96@mail.ru>
2016-11-21 01:43:19 +0500

    сервак подъехал

---
+++ backend/BezPaleva/src/main/java/com/bezPalevaServer/BezPalevaApplication.java
@@ -0,0 +1,12 @@
+package com.bezPalevaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezPalevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezPalevaApplication.class, args);
+	}
+}
