0d8bf3d4751d3b9dc898f3974ed6cd65dda556ab
Adgam <adgam96@mail.ru>
2016-11-27 21:08:09 +0500

    DB2

--- BezPalevaDB.sql
+++ BezPalevaDB.sql
@@ -9,6 +9,20 @@
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Дамп структуры для таблица bezPaleva.Mark
+CREATE TABLE IF NOT EXISTS `Mark` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `photo_path` varchar(100) DEFAULT NULL,
+  `death_time` datetime NOT NULL,
+  `irrelevance_level` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- Экспортируемые данные не выделены.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
