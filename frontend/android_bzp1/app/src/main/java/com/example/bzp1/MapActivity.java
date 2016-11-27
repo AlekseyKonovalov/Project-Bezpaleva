a701c1c74f587333181f645fffad7bac59cbb535
AlekseyKonovalov <konov.95@mail.ru>
2016-11-27 18:54:53 +0500

    работает карта

--- frontend/android_bzp1/app/src/main/java/com/example/bzp1/MapActivity.java
+++ frontend/android_bzp1/app/src/main/java/com/example/bzp1/MapActivity.java
@@ -3,13 +3,32 @@ package com.example.bzp1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ru.yandex.yandexmapkit.*;
+
+
 public class MapActivity extends AppCompatActivity {
+    MapController mMapController;
+
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.map_layout);
+       setContentView(R.layout.map_layout);
+
+        final MapView mapView = (MapView) findViewById(R.id.map);
+//        mapView.showBuiltInScreenButtons(true);
+        mMapController = mapView.getMapController();
+        // determining the user's location
+        mMapController.getOverlayManager().getMyLocation().setEnabled(true);
+
+        // Изменяем зум
+        mMapController.setZoomCurrent(14);
     }
+
+
 }
+
 
 
