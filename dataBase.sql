8282a3ec3beb8dd4061d50ecbc23cbeb54f230ae
Adgam <adgam96@mail.ru>
2016-12-11 00:57:18 +0500

    новая база данных

--- dataBase.sql
+++ dataBase.sql
@@ -20,10 +20,36 @@ CREATE TABLE IF NOT EXISTS `Mark` (
   `photo_path` varchar(100) DEFAULT NULL,
   `death_time` datetime NOT NULL,
   `irrelevance_level` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Mark_User_id_fk` (`user_id`),
+  CONSTRAINT `Mark_User_id_fk` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Экспортируемые данные не выделены.
+-- Дамп данных таблицы bezPaleva.Mark: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `Mark` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Mark` ENABLE KEYS */;
+
+
+-- Дамп структуры для таблица bezPaleva.User
+CREATE TABLE IF NOT EXISTS `User` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `nickname` varchar(100) DEFAULT NULL,
+  `rating` double DEFAULT '0',
+  `photo_path` varchar(100) DEFAULT NULL,
+  `vk_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы bezPaleva.User: ~3 rows (приблизительно)
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` (`id`, `first_name`, `last_name`, `nickname`, `rating`, `photo_path`, `vk_id`) VALUES
+	(1, 'dsfsd', 'dsfds', NULL, 0, NULL, 72245),
+	(2, 'asfasf', 'fsaf', NULL, 0, NULL, 444),
+	(3, 'Чувак', 'Чуваков', NULL, 0, NULL, 5415845);
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
